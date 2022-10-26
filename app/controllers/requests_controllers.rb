29138759c4b3aa6550e9a65acd7a066beab8fa5c
Ethan Wu <ew2664@columbia.edu>
2022-10-25 20:31:42 -0400

    Set up nested routing for requests under listings

---
+++ app/controllers/requests_controllers.rb
@@ -0,0 +1,18 @@
+class RequestsController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def mine
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  resources :users, only: [:new, :create, :index, :show]
-  resources :listings, only: [:new, :create, :index, :show]
+  resources :users, only: [:show, :index, :new, :create]
+  resources :listings, only: [:show, :index, :new, :create] do
+    resources :requests, only: [:show, :index, :new, :create]
+  end
+
+  get "/requests", to: "requests#mine", as: :my_requests
 
   get "/login", to: "sessions#new", as: :login
   post "/login", to: "sessions#create"
