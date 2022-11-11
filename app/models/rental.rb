7b306887a77f8665f88aa74ac66de2f7790a5cda
Ethan Wu <ew2664@columbia.edu>
2022-11-11 13:09:39 -0500

    Add method to convert request into rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,7 +3,11 @@ class Rental < ApplicationRecord
   belongs_to :request, class_name: "RentalRequest"
   belongs_to :renter, class_name: "User"
 
+  validates :status, presence: true
   validate :consistent_with_request
+
+  enum status: [:upcoming, :ongoing, :completed]
+  enum payment_method: [:venmo, :cash, :paypal]
 
   def consistent_with_request
     errors.add(:listing, "must be same as requested listing") if listing_id != request.listing.id
