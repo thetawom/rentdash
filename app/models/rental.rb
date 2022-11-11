c61aa3ef840a8dc3b66b41531920e0cc3a6996f7
Ethan Wu <ew2664@columbia.edu>
2022-11-11 12:57:08 -0500

    Add foreign key validation for rentals

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,13 @@
 class Rental < ApplicationRecord
-  belongs_to :listing_id
-  belongs_to :request_id
-  belongs_to :renter
+  belongs_to :listing
+  belongs_to :request, class_name: "RentalRequest"
+  belongs_to :renter, class_name: "User"
+
+  validate :consistent_with_request
+
+  def consistent_with_request
+    errors.add(:listing, "must be same as requested listing") if listing_id != request.listing.id
+    errors.add(:renter, "must be same as requester") if renter_id != request.requester.id
+  end
+
 end
