22b1b7a0fde13cd5885e8dffa263bde986d125f2
Ethan Wu <ew2664@columbia.edu>
2022-11-15 00:12:34 -0500

    Add unit tests for rentals helper methods

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,11 +1,15 @@
 module ListingsHelper
+
   def fee_unit_options(listing = nil)
     options_for_select(Listing.fee_units.map{|key, value| [key.titleize, key]}, listing&.fee_unit)
   end
+
   def fee_time_options(listing = nil)
     options_for_select(Listing.fee_times.map{|key, value| [key.titleize, key]}, listing&.fee_time)
   end
+
   def item_category_options(listing = nil)
     options_for_select(Listing.item_categories.map{|key, value| [key.titleize, key]}, listing&.item_category)
   end
+
 end
--- app/helpers/rentals_helper.rb
+++ app/helpers/rentals_helper.rb
@@ -1,8 +1,11 @@
 module RentalsHelper
+
   def payment_method_options(rental = nil)
     options_for_select(Rental.payment_methods.map{|key, value| [key.titleize, key]}, rental&.payment_method)
   end
+
   def status_options(rental = nil)
     options_for_select(Rental.statuses.map{|key, value| [key.titleize, key]}, rental&.status)
   end
+
 end
