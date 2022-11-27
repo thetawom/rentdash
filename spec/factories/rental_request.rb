6e0a45469dccd23ae20409f215b9c19fa1faf0f9
catw101 <catelenwu@gmail.com>
2022-11-27 16:43:06 -0500

    Fix unit tests for date validation

--- spec/factories/rental_request.rb
+++ spec/factories/rental_request.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :rental_request do
-      pick_up_time { "2022-10-28 00:00:00 UTC" }
-      return_time {"2022-10-29 00:00:00 UTC"}
+      pick_up_time { DateTime.now + 1.day }
+      return_time { DateTime.now + 2.day}
       payment_method { 1 }
       association :listing, factory: :listing
       association :requester, factory: :user
