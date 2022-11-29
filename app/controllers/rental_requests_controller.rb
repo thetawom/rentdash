d294f8d2a5493d7ff3ff9f6202a3a9549bb09cf7
Nathan <nn2505@columbia.edu>
2022-11-28 22:39:36 -0500

    implemented calculated estimated cost button

--- app/controllers/rental_requests_controller.rb
+++ app/controllers/rental_requests_controller.rb
@@ -16,18 +16,29 @@ class RentalRequestsController < ApplicationController
   def new
     @rental_request = RentalRequest.new
     @rental_request.update rental_request_params if params.has_key? :rental_request
+    @show_estimated_cost = true if params.has_key? :cost
+    @estimated_cost = params[:cost] if params.has_key? :cost
   end
 
   def create
-    @rental_request = RentalRequest.new rental_request_params
-    @rental_request.listing = @listing
-    @rental_request.requester = current_user
-    @rental_request.save
-    
-    if @rental_request.valid?
-      redirect_to listing_rental_requests_path @listing.id
+    if params[:calculate_estimated_cost]
+      @pick_up_time = params[:rental_request][:pick_up_time].to_datetime
+      @return_time = params[:rental_request][:return_time].to_datetime
+      @listing = Listing.find_by(id: params[:listing_id])
+      puts(@listing.id)
+      @estimated_cost = calculate_estimated_cost(@pick_up_time, @return_time, @listing)
+      redirect_to new_listing_rental_request_path @listing.id, rental_request: rental_request_params, cost: @estimated_cost
     else
-      redirect_to new_listing_rental_request_path @listing.id, rental_request: rental_request_params
+      @rental_request = RentalRequest.new rental_request_params
+      @rental_request.listing = @listing
+      @rental_request.requester = current_user
+      @rental_request.save
+      
+      if @rental_request.valid?
+        redirect_to listing_rental_requests_path @listing.id
+      else
+        redirect_to new_listing_rental_request_path @listing.id, rental_request: rental_request_params
+      end
     end
   end
 
@@ -37,18 +48,29 @@ class RentalRequestsController < ApplicationController
       redirect_to listing_rental_requests_path @listing.id
     end
     @rental_request.update rental_request_params if params.has_key? :rental_request
+    @show_estimated_cost = true if params.has_key? :cost
+    @estimated_cost = params[:cost] if params.has_key? :cost
   end
 
   def update
-    if @rental_request.status != "pending"
-      redirect_to listing_rental_requests_path @listing.id
+    if params[:calculate_estimated_cost]
+      @pick_up_time = params[:rental_request][:pick_up_time].to_datetime
+      @return_time = params[:rental_request][:return_time].to_datetime
+      @rental_request = RentalRequest.find_by(id: params[:id])
+      @listing = Listing.find_by(id: @rental_request.listing_id.to_s)
+      @estimated_cost = calculate_estimated_cost(@pick_up_time, @return_time, @listing)
+      redirect_to edit_rental_request_path @rental_request.id, rental_request: rental_request_params, cost: @estimated_cost
     else
-      @rental_request.update rental_request_params
-      if @rental_request.valid?
-        flash[:success] = "Request for #{@listing.name} was updated!"
+      if @rental_request.status != "pending"
         redirect_to listing_rental_requests_path @listing.id
       else
-        redirect_to edit_rental_request_path @rental_request.id, rental_request: rental_request_params
+        @rental_request.update rental_request_params
+        if @rental_request.valid?
+          flash[:success] = "Request for #{@listing.name} was updated!"
+          redirect_to listing_rental_requests_path @listing.id
+        else
+          redirect_to edit_rental_request_path @rental_request.id, rental_request: rental_request_params
+        end
       end
     end
   end
@@ -108,4 +130,17 @@ class RentalRequestsController < ApplicationController
     end
   end
 
+  def calculate_estimated_cost(pick_up_time, return_time, listing)
+    @listing_fee_time = listing.fee_time
+    @listing_fee = listing.fee
+    @listing_fee_unit = listing.fee_unit
+    if @listing_fee_time == 'hour'
+      @estimated_cost = ((return_time - pick_up_time)*24).ceil() * @listing_fee
+    elsif @listing_fee_time == 'day'
+      @estimated_cost = (return_time - pick_up_time).ceil() * @listing_fee
+    elsif @listing_fee_time = 'week'
+      @estimated_cost = ((return_time - pick_up_time)/7).ceil() * @listing_fee
+    end
+    return @estimated_cost
+  end
 end
