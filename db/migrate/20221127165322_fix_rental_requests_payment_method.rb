89b69891416f3c3320a50c3837b42adc3d14f9df
catw101 <catelenwu@gmail.com>
2022-12-02 23:55:32 -0500

    Fix db migrations

--- db/migrate/20221127165322_fix_rental_requests_payment_method.rb
+++ db/migrate/20221127165322_fix_rental_requests_payment_method.rb
@@ -1,6 +1,6 @@
 class FixRentalRequestsPaymentMethod < ActiveRecord::Migration[7.0]
   def change
-    change_column :rental_requests, :payment_method, :integer, null: false
+    change_column :rental_requests, :payment_method, 'integer USING CAST(payment_method AS integer)', null: false
     remove_column :rentals, :payment_method
   end
 end
