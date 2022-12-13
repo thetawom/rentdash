70bfc3e52c9b530efc8630a32fc5cd452bd0dc4d
Ethan Wu <ew2664@columbia.edu>
2022-12-13 14:54:24 -0500

    Add unit test for cancelling rentals

--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -21,4 +21,27 @@ RSpec.describe Rental, type: :model do
     end
   end
 
+  describe "#cancel" do
+    let(:owner) { FactoryBot.create(:user, karma: 1000) }
+    let(:renter) { FactoryBot.create(:user, karma: 1000) }
+    let(:listing) { FactoryBot.create(:listing, owner: owner, fee_unit: "karma") }
+    let(:request) { FactoryBot.create(:rental_request, listing: listing, requester: renter) }
+    let!(:rental) { request.approve }
+    it "should update rental status to cancelled" do
+      expect(rental).to receive(:update).with(status: "cancelled")
+      rental.cancel
+    end
+    it "should transfer karma from the listing owner to the renter if the fee unit is karma" do
+      expect(renter).to receive(:add_karma).with(request.cost)
+      expect(owner).to receive(:deduct_karma).with(request.cost)
+      rental.cancel
+    end
+    it "should not transfer karma if the fee unit is dollars" do
+      listing.update fee_unit: "dollars"
+      expect(renter).to_not receive(:add_karma)
+      expect(owner).to_not receive(:deduct_karma)
+      rental.cancel
+    end
+  end
+
 end
